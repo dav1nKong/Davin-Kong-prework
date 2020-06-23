cb19a6570f6bf5d139353b3e1c7c81985e55fd11
Davin Kong <66775564+dav1nKong@users.noreply.github.com>
2020-06-23 03:05:00 -0500

    Add files via upload

---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1894.4">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Monaco; color: #000000; -webkit-text-stroke: #7923e7}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">var name = prompt('What is your name?');</span></p>
+<p class="p1"><span class="s1">var n = name.length;</span></p>
+<p class="p1"><span class="s1">if (n&gt;4)</span></p>
+<p class="p1"><span class="s1">{</span></p>
+<p class="p1"><span class="s1">alert('your name is greater than 4 characters');</span></p>
+<p class="p1"><span class="s1">}</span></p>
+<p class="p1"><span class="s1">else if (n&lt;4)</span></p>
+<p class="p1"><span class="s1">{</span></p>
+<p class="p1"><span class="s1">alert('your name is less than 4 characters');</span></p>
+<p class="p1"><span class="s1">}</span></p>
+<p class="p1"><span class="s1">else</span></p>
+<p class="p1"><span class="s1">{</span></p>
+<p class="p1"><span class="s1">alert(name);</span></p>
+<p class="p1"><span class="s1">}</span></p>
+</body>
+</html>
---
+++ JavaScript_Basics_Assessment/activity-3.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1894.4">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px 'Andale Mono'; color: #000000; -webkit-text-stroke: #262626; background-color: #ffffff; background-color: rgba(255, 255, 255, 0)}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px 'Andale Mono'; color: #000000; background-color: #ffffff; background-color: rgba(255, 255, 255, 0)}
+    p.p3 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px 'Andale Mono'; color: #000000; background-color: #ffffff; background-color: rgba(255, 255, 255, 0); min-height: 14.0px}
+    p.p4 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px 'Andale Mono'; color: #000000; -webkit-text-stroke: #262626; background-color: #ffffff; background-color: rgba(255, 255, 255, 0); min-height: 14.0px}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">const studentList = ['Lisa', 'Maria', 'Jennifer'];</span></p>
+<p class="p1"><span class="s1">for (let i = 0; i &lt; 3; i++){</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>const name = prompt('Please Enter your name');</span></p>
+<p class="p2"><span class="s1"><span class="Apple-converted-space">  </span>studentList.push(name);</span></p>
+<p class="p1"><span class="s1">}</span></p>
+<p class="p2"><span class="s1">for (let i = 0; i &lt; studentList.length; i++){</span></p>
+<p class="p2"><span class="s1"><span class="Apple-converted-space">  </span>const student = studentList[i];</span></p>
+<p class="p2"><span class="s1"><span class="Apple-converted-space">  </span>console.log(student);</span></p>
+<p class="p3"><span class="s1"></span><br></p>
+<p class="p2"><span class="s1">}</span></p>
+<p class="p4"><span class="s1"></span><br></p>
+</body>
+</html>
